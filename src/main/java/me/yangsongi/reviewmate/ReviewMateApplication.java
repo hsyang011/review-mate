381b8da427d4c86f352f94fa13a47a94c7fa30f0
hsyang011 <hsyang011@gmail.com>
2024-12-11 11:13:02 +0900

    init commit

---
+++ src/main/java/me/yangsongi/reviewmate/ReviewMateApplication.java
@@ -0,0 +1,13 @@
+package me.yangsongi.reviewmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yangsongi/reviewmate/ReviewMateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yangsongi.reviewmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
