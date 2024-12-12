54c210f2d756fe9371118e482370925113500ae8
hsyang011 <hsyang011@gmail.com>
2024-12-12 18:24:27 +0900

    리뷰 조회 구현

--- src/main/java/me/yangsongi/reviewmate/repository/ReviewRepository.java
+++ src/main/java/me/yangsongi/reviewmate/repository/ReviewRepository.java
@@ -5,6 +5,7 @@ import me.yangsongi.reviewmate.domain.Review;
 import me.yangsongi.reviewmate.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     boolean existsByProductAndUser(Product product, User user);
     @Query("SELECT r FROM Review r WHERE r.product.id = :productId AND r.id < :cursor ORDER BY r.id DESC")
     List<Review> findReviewsByProductIdWithPaging(Long productId, int cursor, int size);
+
     @Query("SELECT COUNT(r) FROM Review r WHERE r.product.id = :productId")
     int countByProductId(Long productId);
     @Query("SELECT AVG(r.score) FROM Review r WHERE r.product.id = :productId")
